cd62b77d0d5a66c92898f107922824bfc54aca2a
Elijah Cirioli <eli.cirioli@gmail.com>
2024-11-03 12:12:37 -0800

    basic websocket infrastructure

---
+++ src/app/socket/route.ts
@@ -0,0 +1,14 @@
+import { Server } from "socket.io";
+import { NextRequest, NextResponse } from "next/server";
+
+// export async function GET(request: NextRequest) {
+//     if (res.socket.server.io) {
+// 		console.log("Socket is already running");
+// 	} else {
+// 		console.log("Socket is initializing");
+// 		const io = new Server(res.socket.server);
+// 		res.socket.server.io = io;
+// 	}
+// 	res.end();
+//     new NextResponse().
+// }
