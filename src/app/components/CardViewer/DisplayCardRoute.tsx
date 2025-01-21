9159c92508d1502483c81a158fc51e3341d9e72f
Elijah Cirioli <eli.cirioli@gmail.com>
2025-01-20 19:08:57 -0800

    Basic styling for cards

--- src/app/components/CardViewer/DisplayCardRoute.tsx
+++ src/app/components/CardViewer/DisplayCardRoute.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ReactElement, useEffect, useState } from "react";
 import { Arrival } from "@/lib/models/Arrival";
 import { Route } from "@/lib/models/Route";
 import styles from "./DisplayCardRoute.module.css";
@@ -11,22 +12,45 @@ interface DisplayCardRouteProps {
 }
 
 const DisplayCardRoute: React.FC<DisplayCardRouteProps> = ({ route, arrivals, collapsed }: DisplayCardRouteProps) => {
+	const [currentTime, setCurrentTime] = useState(Date.now());
+
+	useEffect(() => {
+		// Update the current time every second so that we can update the cards without actually receiving fresh data
+		const interval = setInterval(() => setCurrentTime(Date.now()), 1000);
+		return () => {
+			clearInterval(interval);
+		};
+	});
+
+	// TODO: filter out really far away times and also handle when there are no results
 	const relevantArrivals = collapsed ? arrivals.slice(0, 1) : arrivals.slice(0, 3);
 
-	function prettyTime(arrivalTime: number): string {
-		const diffMinutes = Math.round((arrivalTime - Date.now()) / 60000);
+	function arrivalTime(arrivalTime: number): ReactElement {
+		const diffMinutes = Math.max(Math.round((arrivalTime - currentTime) / 60000), 0);
 		if (diffMinutes < 60) {
-			return `${diffMinutes}min`;
+			return (
+				<p className={styles.arrivalTime}>
+					{`${diffMinutes}`}
+					<span className={styles.smallText}>min</span>
+				</p>
+			);
 		}
 		const diffHours = Math.floor(diffMinutes / 60);
-		return `${diffHours}hr${diffMinutes % 60}min`;
+		return (
+			<p className={styles.arrivalTime}>
+				{`${diffHours}`}
+				<span className={styles.smallText}>hr</span>
+				{`${diffMinutes % 60}`}
+				<span className={styles.smallText}>min</span>
+			</p>
+		);
 	}
 
 	return (
 		<div className={styles.routeWrap}>
 			{relevantArrivals.map((arrival) => (
 				<div className={styles.arrival} key={arrival.time}>
-					<p className={styles.arrivalTime}>{prettyTime(arrival.time)}</p>
+					{arrivalTime(arrival.time)}
 					<p className={styles.arrivalDestination}>{route.destination}</p>
 				</div>
 			))}
